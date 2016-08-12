981dd9d1f9e3be90182d4012a658f53f75bb23f2
Vova <faeruner@gmail.com>
2016-08-12 20:20:03 +0300

    added maven-tomcat plugin reference in web.xml

--- Services/src/main/java/by/pvt/module3/command/AdminCommand.java
+++ Services/src/main/java/by/pvt/module3/command/AdminCommand.java
@@ -6,7 +6,6 @@ import by.pvt.module3.resource.ConfigurationManager;
 
 public class AdminCommand implements ActionCommand {
 
-	@Override
 	public String execute(HttpServletRequest request) {
 		String page = null;
 		page = ConfigurationManager.getProperty("path.page.main");
